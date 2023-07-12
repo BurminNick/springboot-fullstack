696ff09543396967b63e52d7881e1255db99567a
BurminNick <BurminNV@tretyakov.ru>
2023-07-12 16:32:05 +0300

    New React Routes

--- frontend/react/src/components/login/Login.jsx
+++ frontend/react/src/components/login/Login.jsx
@@ -92,7 +92,7 @@ const Login = () => {
     const navigate = useNavigate();
     useEffect(()=>{
         if(customer){
-            navigate("/dashboard");
+            navigate("/dashboard/customers");
         }
     })
 
